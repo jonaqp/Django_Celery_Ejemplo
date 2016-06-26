f003455bea48ad865c8125debca83e9972601e8c
Jonathan quiza <jony327@gmail.com>
2016-06-26 13:39:16 -0500

    adding

--- api/bucket.py
+++ api/bucket.py
@@ -1,4 +1,5 @@
 import boto.s3.connection
+import json
 from api.models import Boat, Trip, Image
 from api.utils.funct_dates import (
     convert_str_in_datetime, convert_unix_in_datetime)
@@ -9,6 +10,15 @@ AWS_STORAGE_BUCKET_NAME = 'shellcatch'
 AWS_S3_CUSTOM_DOMAIN = 'https://{0:s}.s3.amazonaws.com/'.format(str(AWS_STORAGE_BUCKET_NAME))
 AWS_ACCESS_KEY_ID = 'AKIAJRA5Z7OCTBF7JPNA'
 AWS_SECRET_ACCESS_KEY = '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'
+
+
+def get_connection_bucket():
+    con = boto.connect_s3(
+        aws_access_key_id=AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+        calling_format=boto.s3.connection.OrdinaryCallingFormat(),
+    )
+    return con
 
 
 def get_validate_format(string_picture):
@@ -57,11 +67,12 @@ def get_validate_format(string_picture):
 
 
 def create_append_image(trip_obj, picture_format, path_dst):
+    time = str(picture_format['datetime'].time())
     latitude = str(picture_format['latitude'])
     longitude = str(picture_format['longitude'])
     battery = str(picture_format['battery'])
     image_ = Image(image_filepath=path_dst, latitude=latitude, longitude=longitude,
-                   orientation='', battery_level=battery)
+                   orientation='', battery_level=battery, time=time)
     trip_obj.image.append(image_)
     trip_obj.save()
 
@@ -71,31 +82,54 @@ def create_trip(picture, path_dst):
     mac_address = picture_format['mac_address']
     date_time = picture_format['datetime']
     date_ = str(date_time.date())
-    time_ = str(date_time.time())
     boat = Boat.objects.filter(mac_address=mac_address)
 
     if not boat:
-        print("here1")
-        trip = Trip.create(date_, time_, mac_address, is_new=True)
+        trip = Trip.create(date_, mac_address, is_new=True)
         create_append_image(trip, picture_format, path_dst)
     else:
-        print("here2")
         boat = Boat.objects.get(mac_address=mac_address)
-        trip = Trip.create(date_, time_, boat, is_new=False)
+        trip = Trip.create(date_, boat, is_new=False)
         create_append_image(trip, picture_format, path_dst)
     return picture_format
 
 
+def create_json_file(data, image_directory):
+    print("Create json")
+    json_file = json.dumps(data, indent=4)
+    conn = get_connection_bucket()
+    bucket_src = conn.get_bucket('shellcatch')
+    src = 'media/uploads/container/'
+    name_directory = image_directory
+    file = "{0}.json".format(str(name_directory))
+    dst = "{0}/{1}/{2}.json".format(str(src), str(name_directory), str(name_directory))
+
+    key = bucket_src.get_key('perl_poetry.pdf')
+    key.get_contents_to_filename('media/uploads/container/perl_poetry.pdf')
+
+        # src = 'media/uploads/container'
+    # name_directory = image_directory
+    # dst = "{0}/{1}/{2}.json".format(str(src), str(name_directory), str(name_directory))
+    # folders = bucket_src.list(dst)
+    # for k in folders:
+    #     if k.name:
+    #         bucket_src.lookup(k.name)
+    #         bucket_src.delete_key(k.name)
+    #         with open(dst, 'wb+') as destination:
+    #             destination.write(json_file)
+    #         key = bucket_src.new_key(destination)
+    #         key.set_contents_from_filename(dst)
+
+
 def load_image():
-    conn = boto.connect_s3(
-        aws_access_key_id=AWS_ACCESS_KEY_ID,
-        aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-        calling_format=boto.s3.connection.OrdinaryCallingFormat(),
-    )
+    conn = get_connection_bucket()
     bucket_src = conn.get_bucket('shellcatch')
     src = 'media/uploads/container/temp/'
     dst = 'media/uploads/container/'
     folders = bucket_src.list(prefix=src, delimiter='.jpg')
+    image_directory = ''
+    result_point = list()
+
     for k in folders:
         extension_correct = k.name.endswith(('.jpg', '.jpeg'))
         if extension_correct:
@@ -103,10 +137,17 @@ def load_image():
             image_directory = k.name.split("/")[-2]
             path_dst = "{0}{1}/{2}".format(str(dst), str(image_directory), str(image_name))
             name = k.name
-            create_trip(name, path_dst)
+            create_trip_ = create_trip(name, path_dst)
+            result_point.append(dict(longitude=str(create_trip_['longitude']),
+                                     latitude=str(create_trip_['latitude'])))
             bucket_src.lookup(k.name)
             bucket_src.copy_key(path_dst, bucket_src.name, k.name)
             bucket_src.delete_key(k.name)
+
+    if result_point:
+        if image_directory:
+            create_json_file(result_point, image_directory)
+
     return str('Successfull')
 
 
