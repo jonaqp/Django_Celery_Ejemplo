2c77727443e72373a6fd3eba5a9d2ada258472cd
Jonathan quiza <jony327@gmail.com>
2016-07-01 00:37:12 -0500

    run task local

--- api/run_local.py
+++ api/run_local.py
@@ -5,14 +5,16 @@ from api.tasks import (get_temp_list_folder, task_create_json_file, create_trip,
 
 
 def run_folder():
-    print("ejecution")
     temp_list = get_temp_list_folder()
     list_folder = temp_list['folder_list']
     bucket_src = temp_list['bucket_src']
+    now = datetime.datetime.now()
+    logger.info("Task Start: result = %s" % str(now))
     create_trip(list_folder)
     task_create_json_file(list_folder, bucket_src)
     task_create_file_csv(list_folder, bucket_src)
     task_move_parent_directory(list_folder, bucket_src)
+    logger.info("Task finished: result = %s" % str(now))
 
 
 if __name__ == '__main__':
